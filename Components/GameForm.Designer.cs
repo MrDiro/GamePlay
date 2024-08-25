da2537c10068859a0cf2ede90bedb8feb65b31dd
MrDiro <cdairo2204@gmail.com>
2024-08-25 09:14:53 -0500

    Agregar archivos de proyecto.

---
+++ Components/GameForm.Designer.cs
@@ -0,0 +1,21 @@
+﻿using GamePlay.Core;
+
+namespace GamePlay.Components
+{
+    partial class GameForm
+    {
+        private void InitializeComponent()
+        {
+            Text = "Pixel Adventure";
+            ClientSize = new System.Drawing.Size(800, 450);
+            BackColor = Color.Black;
+            StartPosition = FormStartPosition.CenterScreen;
+            WindowState = FormWindowState.Maximized;
+            FormBorderStyle = FormBorderStyle.None;
+            DoubleBuffered = true;
+
+            GEngine engine = GEngine.GetGEngineInstance();
+            Paint += engine.GetPaint;
+        }
+    }
+}
---
+++ Core/GEngine.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace GamePlay.Core
+{
+    internal class GEngine
+    {
+        private static readonly Lazy<GEngine> instance = new(() => new GEngine());
+        private readonly PaintEventHandler paint;
+
+        private GEngine() {
+            paint = new(GamePaint);
+        }
+
+        public static GEngine GetGEngineInstance()
+        {
+            return instance.Value;
+        }
+
+        private void GamePaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.SmoothingMode = SmoothingMode.None;
+            g.InterpolationMode = InterpolationMode.NearestNeighbor;
+            g.PixelOffsetMode = PixelOffsetMode.None;
+
+            g.FillRectangle(Brushes.Red, new Rectangle(50, 50, 100, 100));
+        }
+
+        public PaintEventHandler GetPaint
+        {
+            get { return paint; }
+        }
+    }
+}
